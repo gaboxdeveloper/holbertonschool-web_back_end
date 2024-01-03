6637cba71d0d9bfe7e4b33ac9ed0a54b871506ff
Gabriel <grossy.sjb@gmail.com>
2024-01-03 00:32:09 +0000

    task100

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -3,7 +3,6 @@
 from typing import Sequence, Any, Optional
 
 
-# The types of the elements of the input are not known
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """safe_first_element function"""
     if lst:
