246b9784b8ffe64fafa11eba9a1de39fb3ad9c52
Gabriel <grossy.sjb@gmail.com>
2024-01-03 19:07:24 +0000

    task3 import order

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """3. Tasks, write a function task_wait_random that takes an integer"""
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
