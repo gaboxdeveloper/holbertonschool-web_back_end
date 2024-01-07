7c81aa51fdb57e83abc02afcc5d438812efddcb1
Gabriel <grossy.sjb@gmail.com>
2024-01-07 14:53:45 +0000

    task0 v2

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,8 +8,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    ''' Generator that yields a random value between 0 and 10 every second,
-        10 times. '''
-    for i in range(10):
+    """async_generator"""
+    for _ in range(10):
         await sleep(1)
         yield 10 * random()
