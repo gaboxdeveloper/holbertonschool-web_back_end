64cc10fbf1695cfe9da7ebbb121448e9977769f3
Gabriel Rossy <grossy.sjb@gmail.com>
2024-01-08 23:09:07 +0000

    task8 v3

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" List all documents in Python"""
+"""List all documents in Python"""
 
 
 def list_all(mongo_collection):
     """Function that lists all documents in a collection"""
     if not mongo_collection:
         return []
-    return db.mongo_collection.find()
+    return mongo_collection.find()
