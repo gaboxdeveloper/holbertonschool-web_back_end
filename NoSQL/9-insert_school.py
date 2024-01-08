7eb3460a448abfc51e8b9c2c08acb3780eff9ab1
Gabriel Rossy <grossy.sjb@gmail.com>
2024-01-08 23:57:34 +0000

    task9 v2

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -4,4 +4,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """insert school function"""
-    return mongo_collection.insert_one(kwargs)
+    new_doc = mongo_collection.insert_one(kwargs)
+    return new_doc.inserted_id
