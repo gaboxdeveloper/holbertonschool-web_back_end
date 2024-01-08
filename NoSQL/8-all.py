5dee39a744de3861fd4a6ad8bdf26d9e023be374
Gabriel Rossy <grossy.sjb@gmail.com>
2024-01-08 23:07:48 +0000

    task8 v2

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -2,11 +2,8 @@
 """ List all documents in Python"""
 
 
-if __name__ == "__main__":
-    """Function initial"""
-    from pymongo import MongoClient
-
-    def list_all(mongo_collection):
-        """[Function that lists all documents in a collection]
-        """
-        return mongo_collection.find()
+def list_all(mongo_collection):
+    """Function that lists all documents in a collection"""
+    if not mongo_collection:
+        return []
+    return db.mongo_collection.find()
