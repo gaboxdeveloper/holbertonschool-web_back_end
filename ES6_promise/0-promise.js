b03cc0e62ae63b894ec203c66eff4c8e30172c34
Gabriel Rossy <grossy.sjb@gmail.com>
2024-01-25 16:20:14 +0000

    task0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,18 +1,3 @@
 export default function getResponseFromAPI() {
   return new Promise(() => {});
 }
-
-
-
-// Return a Promise using this prototype function getResponseFromAPI()
-
-// bob@dylan:~$ cat 0-main.js
-// import getResponseFromAPI from "./0-promise.js";
-
-// const response = getResponseFromAPI();
-// console.log(response instanceof Promise);
-
-// bob@dylan:~$ 
-// bob@dylan:~$ npm run dev 0-main.js 
-// true
-// bob@dylan:~$ 
