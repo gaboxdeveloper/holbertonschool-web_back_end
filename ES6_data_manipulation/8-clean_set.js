fccb93b79c498cd762c9e90abd4dd5892020a2b3
Gabriel Rossy <grossy.sjb@gmail.com>
2024-02-01 17:14:13 +0000

    task8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result += value.slice(startString.length) + '-';
+    }
+  });
+  return result.slice(0, -1);
+}
