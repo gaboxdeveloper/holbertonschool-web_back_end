9c5803f76e90008a512aca308853f46332874a3c
Gabriel Rossy <grossy.sjb@gmail.com>
2024-02-01 17:38:18 +0000

    task10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,7 @@
 export default function updateUniqueItems(groceries) {
   if (!(groceries instanceof Map)) throw TypeError('Cannot process');
   for (const [item, count] of groceries) {
-	if (count === 1) groceries.set(item, 100);
+    if (count === 1) groceries.set(item, 100);
   }
   return groceries;
 }
